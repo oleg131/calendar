bce3063959b00d51427981a5d2094483794d0154
Oleg Kim <oleg131@mail.ru>
2019-08-23 12:00:14 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import moment from 'moment';
+
+const year = '2016';
+console.log(moment(year).startOf('year').format('DD-MM-YYYY'))
+
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (v, i) =>
+    arr.slice(i * size, i * size + size)
+  );
+
 function App() {
+  var a = moment('2013-01-01');
+  var b = moment(a).add(1, 'months');
+
+  var days = [];
+  for (var m = moment(a); m.isBefore(b); m.add(1, 'days')) {
+    days.push(moment(m))
+  }
+
+  var nBefore = days[0].weekday();
+  var nAfter = 6 - days[days.length - 1].weekday();
+
+  days.unshift(...[...Array(nBefore).keys()].map(() => null));
+  days.push(...[...Array(nAfter).keys()].map(() => null));
+
+  function renderMonth(start) {
+    var end = moment(start).add(1, 'months');
+
+    var days = [];
+    for (var m = moment(start); m.isBefore(end); m.add(1, 'days')) {
+      days.push(moment(m))
+    }
+
+    var nBefore = days[0].weekday();
+    var nAfter = 6 - days[days.length - 1].weekday();
+
+    days.unshift(...[...Array(nBefore).keys()].map(() => null));
+    days.push(...[...Array(nAfter).keys()].map(() => null));
+
+    
+  }
+
+  var month = (
+    chunk(days, 7).map((row, index) => {
+      return (
+        <tr key={ index }>
+          {
+            row.map((day, index) => {
+              return (!!day ?
+                <td class="day" key={ index }>
+                  <div class="day-content">{ day.date() }</div>
+                </td> :
+                <td class="day old" key={ index } />
+              )
+            })
+          }
+        </tr>
+      )
+    })
+  )
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table class="month">
+        <thead>
+          <tr>
+            <th class="month-title" colspan="7">
+              January
+            </th>
+          </tr>
+          <tr>
+            <th class="day-header">Su</th>
+            <th class="day-header">Mo</th>
+            <th class="day-header">Tu</th>
+            <th class="day-header">We</th>
+            <th class="day-header">Th</th>
+            <th class="day-header">Fr</th>
+            <th class="day-header">Sa</th>
+          </tr>
+        </thead>
+        <tbody>
+          { month }
+        </tbody>
+      </table>
     </div>
   );
 }
